05445c495e00f49845870bc6cf146856aef2307e
vinit <vinithashokunal@gmail.com>
2024-11-12 08:59:35 +0530

    TC_001 committed

---
+++ src/test/java/sample/TC_001_AddProductToCart.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class TC_001_AddProductToCart 
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("TC_001");
+
+	}
+
+}
